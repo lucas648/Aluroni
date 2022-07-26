05ec76cb1412023a0073a033e31af19ab037df77
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-07-26 18:21:49 -0300

    criando o projrto Aluroni com inicio da página d ecardápio

---
+++ src/pages/cardapio/index.tsx
@@ -0,0 +1,25 @@
+import CardapioStyles from './Cardapio.module.scss';
+import {ReactComponent as Logo} from 'assets/logo.svg';
+import Buscador from './buscador';
+import { useState } from 'react';
+
+export default function Cardapio(){
+  const [busca, setBusca] = useState('');
+
+  return(
+    <main>
+      <nav className={CardapioStyles.menu}>
+        <Logo/>
+      </nav>
+      <header className={CardapioStyles.header}>
+        <div className={CardapioStyles.header__text}>
+          A casa das Massas
+        </div>
+      </header>
+      <section className={CardapioStyles.cardapio}>
+        <h3 className={CardapioStyles.titulo}>Cardapio</h3>
+        <Buscador busca={busca} setBusca={setBusca}/>
+      </section>
+    </main>
+  )
+}
