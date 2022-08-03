3f97ec9a98cc3f686d534674fac876c24e9faa65
Lucas Costa e Silva Almeida <lucas.almeida@compasso.com.br>
2022-08-03 11:39:04 -0300

    refatorando a página de pratos

---
+++ src/routes.tsx
@@ -0,0 +1,30 @@
+import Footer from 'components/footer'
+import Home from 'components/home'
+import Menu from 'components/menu'
+import Cardapio from 'pages/cardapio'
+import Inicio from 'pages/inicio'
+import NotFound from 'pages/notFound'
+import Prato from 'pages/prato'
+import Sobre from 'pages/sobre'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+export default function AppRouter(){
+  return(
+    <main className='container'>
+      <Router>
+        <Menu/>
+        <Routes>
+          <Route path='/' element={<Home/>} >
+            <Route index element={<Inicio/>} />
+            <Route path='cardapio' element={<Cardapio/>} />
+            <Route path='sobre' element={<Sobre/>} />
+          </Route>
+          <Route path='prato/:id' element={<Prato/>}/>
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+        <Footer/>
+      </Router>
+    </main>
+
+  )
+}
